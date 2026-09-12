5da5e4a4b6e400d8ba673cd4a72b4e253356e9ac
StanneLLC <189099+StanneLLC@users.noreply.github.com>
2026-09-12 15:27:12 -0400

    Add continuous Number Push difficulty model

---
+++ LevelGenerator/NumberPushDifficulty.cs
@@ -0,0 +1,21 @@
+﻿namespace NovaWright.NumberPush.LevelGenerator
+{
+    public class NumberPushDifficulty
+    {
+        public int LevelNumber { get; }
+
+        public NumberPushDifficulty(
+            int levelNumber)
+        {
+            if (levelNumber <= 0)
+            {
+                throw new ArgumentOutOfRangeException(
+                    nameof(levelNumber),
+                    "Level number must be greater than zero.");
+            }
+
+            LevelNumber =
+                levelNumber;
+        }
+    }
+}
