c080d242d56b04773d1595561b2336cfacbc5062
StanneLLC <189099+StanneLLC@users.noreply.github.com>
2026-09-25 15:34:17 -0400

    Revert precomputed successor reachability neighbors

--- LevelGenerator/NumberPushSolver.cs
+++ LevelGenerator/NumberPushSolver.cs
@@ -16,11 +16,6 @@ namespace NovaWright.NumberPush.LevelGenerator
 
         private readonly bool[] blockedCells;
 
-        private readonly int[] leftNeighbors;
-        private readonly int[] rightNeighbors;
-        private readonly int[] upNeighbors;
-        private readonly int[] downNeighbors;
-
         // Reusable arrays for current-state player reachability.
         private readonly int[] reachableVisit;
         private readonly int[] reachableQueue;
@@ -141,18 +136,6 @@ namespace NovaWright.NumberPush.LevelGenerator
             blockedCells =
                 new bool[cellCount];
 
-            leftNeighbors =
-new int[cellCount];
-
-            rightNeighbors =
-            new int[cellCount];
-
-            upNeighbors =
-            new int[cellCount];
-
-            downNeighbors =
-            new int[cellCount];
-
             for (int index = 0;
             index < cellCount;
             index++)
@@ -163,25 +146,6 @@ new int[cellCount];
                 int y =
                     index / columns;
 
-                leftNeighbors[index] =
-                    x > 0
-                        ? index - 1
-                        : -1;
-
-                rightNeighbors[index] =
-                    x < columns - 1
-                        ? index + 1
-                        : -1;
-
-                upNeighbors[index] =
-                    y > 0
-                        ? index - columns
-                        : -1;
-
-                downNeighbors[index] =
-                    y < rows - 1
-                        ? index + columns
-                        : -1;
 
             }
 
